65fc31a005d7e397df122c870a839ee1f6393c1d
soumya_84 <bangaragundsoumya@gmail.com>
2026-01-17 13:37:41 +0530

    Fix indentation and syntax errors in even.py

--- even.py
+++ even.py
@@ -1,6 +1,7 @@
 import sys
-   for arg in sys.argv[1:]:
-      if ( % 2 ) == 0:
-        print("Even number")
-      else:
-        print("odd number")
+ for arg in sys.argv[1:]:
+    
+if ( % 2 ) == 0:
+   print("Even number")
+else:
+   print("odd number")
